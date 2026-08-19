306ae669cb39329afc04344175b2b20597339568
mdadilcc <111047667+mdadilcc@users.noreply.github.com>
2026-08-19 09:11:09 +0000

    second

--- test.js
+++ test.js
@@ -1,8 +1,77 @@
-console.log("test");
-(function() {
-  console.log('This runs immediately!');
-})();
+// console.log("test");
+// (function() {
+//   console.log('This runs immediately!');
+// })();
 
-(function(){
-console.log("iife function without call a fucntion this will work")
-})();
+// (function(){
+// console.log("iife function without call a fucntion this will work")
+// })();
+
+// alert("node test aler")
+// console.log(null)
+// console.log(typeof null)
+
+// let symbolV = Symbol("Unique")
+// console.log(symbolV)
+
+// console.log(typeof "test");
+
+// let NumV = "12ab"
+// // console.log(NumV)
+// // console.log(typeof NumV)
+// let conN = Number(NumV)
+// console.log(conN)
+// console.log(typeof conN)
+
+// let isLoggedIn = 101
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// let str = 33;
+// // console.log(str);
+// // console.log(typeof str);
+// let NumS = String(str);
+// console.log(NumS);
+// console.log(typeof NumS);
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// let b = a++;
+// console.log(a);
+// console.log(b);
+
+// let x =10;
+// let a = x++;
+// console.log("This will Return 10 for a for post increament : " + a);
+// console.log("This will Return 11 for x for post increament:" + x);
+
+// let x =10;
+// let a = ++x;
+// console.log("This will Return 11 for a for pre increament : " + a);
+// console.log("This will Return 11 for x for pree increament:" + x);
+
+// let funVar = function basiFun(){
+//     console.log("This is a basic fucntion")
+// }
+// funVar();
+
+let var1 = "primitive data type like all 7 all the store in a heap momory only refence will get "
+let var2 = var1;
+var2 = "now change the value of var2 beacuse it will save in stack means a refence will give u"
+// console.log(var1);
+console.log(var2);
+
+
+//  Primitive (they will store in stack)
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
